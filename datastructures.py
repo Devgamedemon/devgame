dd1a53bbec16ee9aec42bd9ff854e77d5bd56095
Devgamedemon <devgamedemon@gmail.com>
2023-12-17 00:29:59 +0200

    third commit

--- datastructures.py
+++ datastructures.py
@@ -16,3 +16,28 @@ try:
 except:
     print("Error")
 
+#ti diafora exei to ena me to allo ,na prosthesw kai na afairesw ena stoixeio apo to kathena
+#na ta kanw iterate , na allaxw ena stoixeio pou exei mesa to kathena
+
+x = ["apple", "banana", "cherry"]
+x.append("pear")
+print(x)
+x.remove("pear")
+print(x)
+
+# Python code to iterate over a list
+# Using for loop
+for i in x:
+    print(i)
+
+#alaxw ena stoixeio sth lista pou den xerw thn thesh
+x[1]="pear"
+print(x)
+
+counter=0
+for i in x:
+
+    if i == "pear":
+        x[counter]="banana"
+    counter += 1
+print(x)
