6d86beb1d4c3458f5f7c3cbd1ea1ffc42b186fd9
Devgamedemon <devgamedemon@gmail.com>
2023-12-24 22:44:14 +0200

    askhsh6askmore commited

--- Askhsh6.py
+++ Askhsh6.py
@@ -4,22 +4,37 @@ import re
 
 
 
+# email=[]
+#
+# regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+# cond=False
+# while  cond==False :
+#         cond1=False
+#
+#         email = input("\n Enter a valid email adress: ")
+#         #if email == "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b" :
+#         if re.fullmatch(regex, email):
+#                 print("Valid Email")
+#                 cond=True
+#         else:
+#                 print("Invalid Email")
+
+
+
+#deyteros tropos
 email=[]
-
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 cond=False
-while  cond==False :
+while cond==False :
         cond1=False
-
         email = input("\n Enter a valid email adress: ")
-        #if email == "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b" :
-        if re.fullmatch(regex, email):
-                print("Valid Email")
-                cond=True
+        mailstring = email.split('@')
+        print(mailstring)
+        counter1=email.count("@")
+        print(counter1)
+        if len(mailstring)==2 :
+                counter2 = mailstring[1].count(".")
         else:
-                print("Invalid Email")
-
-
-
-
-
+                print("invalid email")
+                continue
+        if counter2==1 and counter1==2 :
+                print("valid")
---
+++ test.py
@@ -0,0 +1,8 @@
+
+
+test = [1,2,3,4,5,".",7,8,9]
+counter2 = test[5].count(".")
+print(counter2)
+
+
+
